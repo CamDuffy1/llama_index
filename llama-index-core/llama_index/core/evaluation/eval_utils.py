a221bc985b59c278413262e847077dc5eb988f9e
Cameron Duffy <150475466+CamDuffy1@users.noreply.github.com>
2024-02-20 19:56:10 +0000

    updating default_parser to handle eval_response beginning with newline char

--- llama-index-core/llama_index/core/evaluation/eval_utils.py
+++ llama-index-core/llama_index/core/evaluation/eval_utils.py
@@ -175,6 +175,8 @@ def default_parser(eval_response: str) -> Tuple[Optional[float], Optional[str]]:
     Returns:
         Tuple[float, str]: A tuple containing the score as a float and the reasoning as a string.
     """
+    if eval_response[0] == '\n':
+        eval_response = eval_response[1:]
     score_str, reasoning_str = eval_response.split("\n", 1)
     score = float(score_str)
     reasoning = reasoning_str.lstrip("\n")
